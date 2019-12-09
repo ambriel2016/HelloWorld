914a2e5ffd360dcc7be42e0ed8b416c686946d9e
ambriel2016 <crb3305@gmail.com>
2019-12-09 10:30:32 -0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        String myVar = "Cris";
+
+        System.out.println("Hello World");
+        System.out.println(myVar);
+        System.out.println("Hello and Welcome,  " + myVar);
+    }
+}
